5f71e31bd39427e1311f373f1f812979069f80c2
Marina Fedorova <marinafedorova_93@mail.ru>
2025-11-10 17:01:54 +0300

    Добавлены тесты на add_area

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,4 +1,7 @@
 from src.circle import Circle
+from src.triangle import Triangle
+from src.rectangle import Rectangle
+from src.square import Square
 import pytest
 
 
@@ -37,4 +40,22 @@ def test_circle_invalid_sides(radius, expected_exception, text_exception: str):
         Circle(radius)
 
 
+class TestCircleAddArea:
+    def test_circle_add_area(self):
 
+        circle = Circle(2)                     # площадь круга ≈ 12.57
+        triangle = Triangle(3, 4, 5)  # площадь треугольника = 6
+        rectangle = Rectangle(4, 6)      # площадь прямоугольника = 24
+        square = Square(5)                     # площадь квадрата = 25
+        another_circle = Circle(3)             # площадь второго круга ≈ 28.27
+
+        assert circle.add_area(triangle) == 18.57
+        assert circle.add_area(rectangle) == 36.57
+        assert circle.add_area(square) == 37.57
+        assert circle.add_area(another_circle) == 40.84
+
+    def test_circle_add_area_invalid_object(self):
+
+        circle = Circle(4)
+        with pytest.raises(ValueError, match="Should be a Figure"):
+            circle.add_area("invalid object")
